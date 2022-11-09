aa707301fab99e9fd409313e69b84c8ce7cd7249
nolto1sj <sjnolton@gmail.com>
2022-11-09 00:14:08 -0500

    Initial Commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,54 @@
+﻿using BookApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpPost("add")]
+        public Book AddBook(string title, string category)
+        {
+            return Book.AddBook(title, category);
+        }
+
+        [HttpGet]
+        public List<Book> GetAll()
+        {
+            return Book.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Book FindById(int id)
+        {
+            return Book.FindById(id);
+        }
+
+        [HttpGet("category/categoryname")]
+        public List<Book> FindByCategory(string categoryname)
+        {
+            return Book.FindByCategory(categoryname);
+        }
+
+        [HttpGet("search/{keyword}")]
+        public List<Book> FindByKeyword(string keyword)
+        {
+            return Book.FindByKeyword(keyword);
+        }
+
+        [HttpPut("updatebook/{id}")]
+        public void UpdateBook(int id, string title, string category)
+        {
+            Book.UpdateBook(id, title, category);
+        }
+
+        [HttpDelete("deletebook/{id}")]
+        public void DeleteBook(int id)
+        {
+            Book.DeleteBook(id);
+        }
+
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,63 @@
+﻿namespace BookApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+        //pseudo database
+        private static List<Book> Library = new List<Book>()
+        {
+            new Book() { Id = 1, Title = "The Odyssey", Category = "Fiction" },
+            new Book() { Id = 2, Title = "Perdido Street Station", Category = "Fiction" }
+        };
+        private static int NextId = 3;
+
+        //pseudo service
+        public static Book AddBook(string title, string category)
+        {
+            var b = new Book
+            {
+                Id = NextId++,
+                Title = title,
+                Category = category
+            };
+            Library.Add(b);
+            return b;
+        }
+
+        public static List<Book> GetAll()
+        {
+            return Library;
+        }
+
+        public static Book FindById(int id)
+        {
+            return Library.First(book => book.Id == id);
+        }
+
+        public static List<Book> FindByCategory(string category)
+        {
+            return Library.FindAll(b => b.Category == category);
+        }
+
+        public static List<Book> FindByKeyword(string keyword)
+        {
+            return Library.FindAll(b => b.Title.ToLower().Contains(keyword.ToLower()));
+        }
+
+        public static void UpdateBook(int id, string title, string category)
+        {
+            var book = Library.SingleOrDefault(b => b.Id == id);
+            book.Title = title;
+            book.Category = category;
+        }
+
+        public static void DeleteBook(int id)
+        {
+            var book = Library.SingleOrDefault(b => b.Id == id);
+            Library.Remove(book);
+        }
+    }
+}
